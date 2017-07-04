c28b1d49e95769f5d4ae918bc9f2dfdbda291f6b
don79is <don79is@gmail.com>
2017-07-04 15:13:52 +0300

    Initial for loop continue

--- src/main/java/antra_paskaita/Main.java
+++ src/main/java/antra_paskaita/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        arrayExamples();
+        forLoopContinue();
     }
 
     public static void sumAlgorithm() {
@@ -207,6 +207,15 @@ public class Main {
                 continue;
             }
             System.out.println();
+        }
+    }
+
+    public static void forLoopContinue() {
+        for (int i = 0; i < 20; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.println(i);
         }
     }
 }
