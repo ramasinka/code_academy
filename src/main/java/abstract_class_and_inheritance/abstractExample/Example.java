40eacebe89bc828e7843d0acfce924a495b45837
don79is <don79is@gmail.com>
2017-07-10 10:19:48 +0300

    Refactor code

--- src/main/java/abstract_class_and_inheritance/abstractExample/Example.java
+++
@@ -1,9 +0,0 @@
-package abstract_class_and_inheritance.abstractExample;
-
-/**
- * Created by CodeAcademy on 2017.07.05.
- */
-public class Example extends AbstractExample {
-    void intro() {
-    }
-}
